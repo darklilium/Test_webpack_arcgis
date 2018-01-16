db15b96c8720ae990936e3351b11271cd1832249
Evelyn Hernández <darklilium@users.noreply.github.com>
2018-01-16 15:32:21 -0300

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+
+  class Main extends React.Component {
+    render(){
+      return (
+        <div>Hola desde React Component</div>
+      );
+    }
+  }
+
+  export default Main;
+
